e9a672bfb5faebde04f4d216ff9e47c7f0d5e480
user <no.email>
2017-08-17 11:31:53 +0200

    Fixing an error and some cosmetics

--- R/links.R
+++ R/links.R
@@ -1,33 +1,3 @@
-## duotrio <- function ()
-## {
-##   duotrio <- binomial()
-##   duotrio$link <- "Link for the duo-trio test"
-##   duotrio$linkinv <- function(eta) {
-##     tres <- 1 - pnorm(eta/sqrt(2)) - pnorm(eta/sqrt(6)) +
-##       2 * pnorm(eta/sqrt(2)) * pnorm(eta/sqrt(6))
-##     tres[eta < 0] <- 0.5
-##     tres
-##   }
-##   duotrio$mu.eta <- function(eta) -dnorm(eta/sqrt(2))/sqrt(2) -
-##     dnorm(eta/sqrt(6))/sqrt(6) + 2 *
-##       (dnorm(eta/sqrt(2)) * pnorm(eta/sqrt(6))/sqrt(2) +
-##        pnorm(eta/sqrt(2)) * dnorm(eta/sqrt(6))/sqrt(6))
-##   duotrio$linkfun <- function(mu) {
-##     duotriog <- function(d, p) duotrio$linkinv(d) - p
-##     tres <- mu
-##     for(i in 1:length(mu)) {
-##       if(mu[i] <= 0.5)
-##         tres[i] <- 0
-##       else if(mu[i] > 1-1e-9)
-##         tres[i] <- Inf
-##       else
-##         tres[i] <- uniroot(duotriog, c(0, 14), p = mu[i])$root
-##     }
-##     tres
-##   }
-##   duotrio
-## }
-
 duotrio <- function() {
   duotrio <- binomial()
   duotrio$link <- "Link for the duo-trio test"
@@ -131,45 +101,6 @@ doubleduotrio <- function() {
   doubleduotrio
 }
 
-
-## `threeAFC` <-
-##   function ()
-## {
-##   threeAFC <- binomial()
-##   threeAFC$link <- "Link for the 3-AFC test"
-##   threeAFC$linkinv <- function(eta) {
-##     threeAFCg <- function(x, d) dnorm(x - d) * pnorm(x)^2
-##     tres <- eta
-##     for (i in 1:length(eta)) {
-##       if (eta[i] > 0)
-##         tres[i] <- integrate(threeAFCg, -Inf, Inf, d = eta[i])$value
-##       else tres[i] <- 1/3
-##     }
-##     tres
-##   }
-##   threeAFC$mu.eta <- function(eta) {
-##     threeAFCgd <- function(x, d) (x - d) * dnorm(x - d) *
-##       pnorm(x)^2
-##     tres <- eta
-##     for (i in 1:length(eta)) tres[i] <-
-##       integrate(threeAFCgd, -Inf, Inf, d = eta[i])$value
-##     tres
-##   }
-##   threeAFC$linkfun <- function(mu) {
-##     threeAFCg2 <- function(d, p) -p + threeAFC$linkinv(d)
-##     tres <- mu
-##     for (i in 1:length(mu)) {
-##       if (mu[i] > 1/3)
-##         res <- uniroot(threeAFCg2, c(0, 10), p = mu[i])$root
-##       if (mu[i] <= 1/3)
-##         res <- 0
-##       tres[i] <- res
-##     }
-##     tres
-##   }
-##   threeAFC
-## }
-
 threeAFC <- function ()
 {
   threeAFC <- binomial()
@@ -258,47 +189,6 @@ doublethreeAFC <- function ()
   }
   doublethreeAFC
 }
-
-
-## `triangle` <-
-##   function ()
-## {
-##   triangle <- binomial()
-##     triangle$link <- "Link for the triangle test"
-##   triangle$linkinv <- function(eta) {
-##     triangleg <- function(x, d) 2 * dnorm(x) *
-##       (pnorm(-x * sqrt(3) + d * sqrt(2/3)) +
-##        pnorm(-x * sqrt(3) - d * sqrt(2/3)))
-##     tres <- eta
-##     for (i in 1:length(eta)) {
-##       if (eta[i] > 0)
-##         tres[i] <- integrate(triangleg, 0, Inf, d = eta[i])$value
-##       else tres[i] <- 1/3
-##     }
-##     tres
-##   }
-##   triangle$mu.eta <- function(eta) {
-##     trianglegd <- function(x, d) 2 * sqrt(2/3) * dnorm(x) *
-##       (dnorm(-x * sqrt(3) + d * sqrt(2/3)) -
-##        dnorm(-x * sqrt(3) - d * sqrt(2/3)))
-##     tres <- eta
-##     for (i in 1:length(eta)) tres[i] <-
-##       integrate(trianglegd, 0, Inf, d = eta[i])$value
-##     tres
-##   }
-##   triangle$linkfun <- function(mu) {
-##     triangleg2 <- function(d, p) -p + triangle$linkinv(d)
-##     tres <- mu
-##     for (i in 1:length(mu)) {
-##       if (mu[i] > 1/3)
-##         tres[i] <- uniroot(triangleg2, c(0, 10), p = mu[i])$root
-##       if (mu[i] <= 1/3)
-##         tres[i] <- 0
-##     }
-##     tres
-##   }
-##   triangle
-## }
 
 triangle <- function()
 {
@@ -407,10 +297,6 @@ tetrad <- function()
     ok <- eta > eps & eta < 9
     eta[eta <= eps] <- 1/3
     eta[eta >= 9] <- 1
-    ## TetradsLinkinv <- function(eta) {
-    ## ok <- eta > -9 & eta < 9
-    ## eta[eta <= 0] <- 1/3
-    ## eta[eta >= 9] <- 1
     if(sum(ok)) {
       tetrads.fun <- function(z, delta)
         dnorm(z) * (2 * pnorm(z) * pnorm(z - delta) -
@@ -470,7 +356,7 @@ doubletetrad <- function()
       eta[ok] <- sapply(eta[ok], function(eta) {
         (1 - 2*integrate(tetrads.fun, -Inf, Inf, delta=eta)$value)^2 })
     }
-    pmin(pmax(eta, 1/3), 1) ## restrict to [1/3, 1] - just to be sure
+    pmin(pmax(eta, 1/9), 1) ## restrict to [1/9, 1] - just to be sure
   }
   doubletetrad$mu.eta <- function(eta) {
     eps <- 1e-8
@@ -504,28 +390,6 @@ doubletetrad <- function()
   }
   doubletetrad
 }
-
-## twoAFC <- function() {
-##   twoAFC <- binomial()
-##   twoAFC$link <- "Link for the 2-AFC test"
-##   twoAFC$linkfun <- function(mu) {
-##     tres <- mu
-##     for (i in 1:length(mu)) {
-##       if (mu[i] > 0.5)
-##         tres[i] <- sqrt(2) * qnorm(mu[i])
-##       if (mu[i] <= 0.5)
-##         tres[i] <- 0
-##     }
-##     tres
-##   }
-##   twoAFC$linkinv <- function(eta) {
-##     tres <- pnorm(eta/sqrt(2))
-##     tres[eta < 0] <- 0.5
-##     tres
-##   }
-##   twoAFC$mu.eta <- function(eta) dnorm(eta/sqrt(2))/sqrt(2)
-##   twoAFC
-## }
 
 twoAFC <- function() {
   twoAFC <- binomial()
@@ -562,7 +426,7 @@ doubletwoAFC <- function() {
     ok <- eta > 0
     eta[!ok] <- 0.25
     eta[ok] <- (pnorm(eta[ok] / sqrt(2)))^2
-    pmin(pmax(eta, 0.25), 1) ## restrict to [0.5, 1] - just to be sure
+    pmin(pmax(eta, 0.25), 1) ## restrict to [0.25, 1] - just to be sure
   }
   doubletwoAFC$mu.eta <- function(eta) {
     eta.linkinv <- eta
@@ -580,14 +444,12 @@ doubletwoAFC <- function() {
     ok <- mu > 0.25 & mu < 1
     mu[mu <= 0.25] <- 0
     mu[mu >= 1] <- Inf
-    # mu[ok] <- 2 * qnorm(mu[ok])^2
-    # pmax(mu, 0) ## delta cannot be negative
     if(sum(ok)) {
       twoafcs <- function(d, p) doubletwoAFC$linkinv(d) - p
       mu[ok] <- sapply(mu[ok], function(mu)
         uniroot(twoafcs, c(0, 9), p = mu)$root)
     }
-    pmax(mu, 0)
+    pmax(mu, 0) ## delta cannot be negative
   }
   doubletwoAFC
 }
